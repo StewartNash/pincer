8ca668244a9f47c295a6363da606dedaeeb443b1
pervigilent <pervigilent@gmail.com>
2025-01-30 02:40:51 -0700

    Started migrating to ROS2

---
+++ build/arctos_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/stewart/Repositories/pincer/install/arctos_urdf_description/share/arctos_urdf_description/package.sh"
---
+++ build/arctos_moveit/rosidl_cmake/rosidl_cmake-extras.cmake
@@ -0,0 +1,4 @@
+# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
+
+set(arctos_moveit_IDL_FILES "msg/ArmJointState.idl")
+set(arctos_moveit_INTERFACE_FILES "msg/ArmJointState.msg")
