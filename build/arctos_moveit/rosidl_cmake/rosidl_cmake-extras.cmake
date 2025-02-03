066c7813ef3e43d8421bfd29bf50f6eb22ba59c5
pervigilent <pervigilent@gmail.com>
2025-02-02 17:54:22 -0700

    Removed autogenerated directories.

--- build/arctos_moveit/rosidl_cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(arctos_moveit_IDL_FILES "msg/ArmJointState.idl")
-set(arctos_moveit_INTERFACE_FILES "msg/ArmJointState.msg")
--- build/arctos_urdf_description/CMakeFiles/arctos_urdf_description_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/arctos_urdf_description_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/arctos_urdf_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
